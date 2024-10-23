3f25c666b33304f3542a58348b78ed8d50d077e8
smillxx <smillerd82@gmail.com>
2024-10-23 09:04:51 -0500

    update backend build

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Culinary Budget API');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
